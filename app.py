33294fc050a80d95923524830a2dbb98c14d0d5e
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-29 13:33:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def main():
           linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)),
           url("https://raw.githubusercontent.com/AyushDodal/Ayush-Dodal/main/images/IMG_6667.JPG");
         background-size: 50%;
+        backgorund-height: 100%
         background-position: center;
         background-repeat: no-repeat;
     }
