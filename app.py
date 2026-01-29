120ac2eaef42970c33a3f2a6f5308cb9958bf14c
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-29 13:32:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
     .stApp {
         background: 
           linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)),
-          url("https://raw.githubusercontent.com/AyushDodal/Ayush-Dodal/main/images/IMG_5533.jpg");
+          url("https://raw.githubusercontent.com/AyushDodal/Ayush-Dodal/main/images/IMG_6667.jpg");
         background-size: 50%;
         background-position: center;
         background-repeat: no-repeat;
