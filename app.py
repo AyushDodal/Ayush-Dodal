e678f664c63613227734ad9c0e66d1084b95ea44
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-29 13:45:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,21 @@ def main():
   )
 
   #st.title("Ayush Dodal")
-  st.markdown('<h1 class="hero">Ayush Dodal</h1>', unsafe_allow_html=True)
+  st.markdown(
+    """
+    <style>
+    .hero-title {
+        font-size: 72px;
+        font-weight: 700;
+        color: white;
+        font-family: Inter, sans-serif;
+        text-align: center;
+        margin-top: 40px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True)
+  st.markdown('<h1 class="hero-title">Ayush Dodal</h1>', unsafe_allow_html=True)
   
 
 
