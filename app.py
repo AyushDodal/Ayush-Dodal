7965c82128287f500209725f4a8ce537ebe6953b
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-29 13:39:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ def main():
     unsafe_allow_html=True
   )
 
-  st.title("Ayush Dodal")
+  #st.title("Ayush Dodal")
+  st.markdown('<h1 class="hero">Ayush Dodal</h1>', unsafe_allow_html=True)
+
 
 if __name__=="__main__":
   main()
