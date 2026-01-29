0ab52096578eb6cdb5da0c8367241b336cad526d
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-29 15:52:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def main():
         font-weight: 700 !important; 
         color: white !important; 
         font-family: 'Courier Prime' !important;
-        position: absolute;
+        position: fixed;
         top: 360px;
         left: -400px; 
         } </style> """, unsafe_allow_html=True) 
