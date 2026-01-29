636735d76db147e1688133043b915240da19f6eb
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-29 12:37:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ import streamlit as st
 
 
 def main():
+  st.markdown(
+    """
+    <style>
+    .stApp {
+        background: 
+          linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),
+          url("images/img.jpg");
+        background-size: cover;
+        background-position: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+  )
 
   st.title("Ayush Dodal")
 
