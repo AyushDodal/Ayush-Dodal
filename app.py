97ca47b1980b543fb530c606049c1f9415895098
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-30 13:50:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,8 +79,7 @@ def main():
         white-space: nowrap;
 
 
-        animation: animate-about;
-        animation-timeline: scroll();
+
         
         
     }
@@ -130,7 +129,7 @@ def main():
     <div class="hero">
         <img src="https://raw.githubusercontent.com/AyushDodal/Ayush-Dodal/main/images/IMG_6667.JPG">
         <h1 class="hero-title">hi, i'm ayush, an<br> ai engineer from mumbai</h1>
-        <p class="hero-about">I like building things that work — data systems, 
+        <p class="hero-about">I like building things that work — <br>data systems, 
         AI tools, and ideas that actually ship.</p>
         
 
