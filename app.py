dbcaa827aab01310376d6c2f7f2635931b4984a2
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-30 13:53:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,10 +129,18 @@ def main():
     <div class="hero">
         <img src="https://raw.githubusercontent.com/AyushDodal/Ayush-Dodal/main/images/IMG_6667.JPG">
         <h1 class="hero-title">hi, i'm ayush, an<br> ai engineer from mumbai</h1>
-        <p class="hero-about">I like building things that work — <br>data systems, 
-        AI tools, and ideas that actually ship.</p>
         
+        <p class="hero-about">I like building things that work — <br>data systems, 
+        ai tools, and ideas that <br>actually ship.</p>
 
+        <p class="hero-edu">pursued my master's from northeastern university in boston.</p>
+
+        <p class="hero-projects-archive">projects archive</p>
+
+        <p class="hero-projects-list">
+        fitness tracking ai agent<br>
+        rag chatbot
+        </p>
         
     </div>
     """,
