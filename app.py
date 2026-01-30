3d9703562b366f2733c9802d4ea703270e5c136f
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-30 10:36:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,8 +89,8 @@ def main():
     """
     <div class="hero">
         <img src="https://raw.githubusercontent.com/AyushDodal/Ayush-Dodal/main/images/IMG_6667.JPG">
-        <h1 class="hero-title">Ayush Dodal</h1>
-        <h2 class="hero-about">About me</h2>
+        <h1 class="hero-title">Hi, I'm Ayush</h1>
+        <h2 class="hero-about">I'm a Data Analyst from Mumbai. Lately I've been learning about AI.</h2>
         
     </div>
     """,
