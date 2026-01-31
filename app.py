5e418e22be10bdc378d1d78daf87cd023f9d8dec
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-31 17:23:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def main():
     .contact{
         position: absolute;
         top: 195vh;
-        left: 90 px;
+        left: 300 px;
         color: black !important;
         font-family: 'Courier Prime', monospace !important;
         font-size: 60px !important;
